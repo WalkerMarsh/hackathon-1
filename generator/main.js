b4b5bb11fc7acbc5eb7b93adaa895f5626207adb
Walker <walkermarsh9@gmail.com>
2021-05-19 20:43:43 -0400

    first draft

--- generator/main.js
+++ generator/main.js
@@ -1,36 +1,66 @@
 
 
 document.getElementsByIdName('sad').addEventListener('click', function () {
-  let url = feelingObj['sad'][Math.floor[Math.random() * 10]]
+  let url = feelingObj['sad'][Math.floor[Math.random() * 1]]
   this.href = url;
 })
 
 document.getElementsByIdName('happy').addEventListener('click', function () {
-  let url = feelingObj['happy'][Math.floor[Math.random() * 10]]
+  let url = feelingObj['happy'][Math.round(Math.random() * 3)]
   this.href = url;
 })
 
 document.getElementsByIdName('IdkYouTellMe').addEventListener('click', function () {
-  let url = feelingObj['IdkYouTellMe'][Math.floor[Math.random() * 10]]
+  let url = feelingObj['IdkYouTellMe'][Math.round(Math.random() * 3)]
   this.href = url;
 })
 
 document.getElementsByIdName('motivated').addEventListener('click', function () {
-  let url = feelingObj['motivated'][Math.floor[Math.random() * 10]]
+  let url = feelingObj['motivated'][Math.round(Math.random() * 3)]
+  this.href = url;
+})
+document.getElementsByIdName('annoyed').addEventListener('click', function () {
+  let url = feelingObj['annoyed'][Math.round[Math.random()]
   this.href = url;
 })
 
+//i feel like instead of an array here it should call a function that does stuff and then goes to the link
 let feelingObj = {
-  'sad': []
-  'happy': [happylinks],
-  'motivated': [sadFeelinglinks],
-  'IdkYouTellMe':
+  'sad': ['https://www.youtube.com/watch?v=IO9d2PpP7tQ',]//currently the ASPCA video
+  'happy': ['https://www.youtube.com/watch?v=W31EBx0swWk'], //currently a video of vets coming home
+  'motivated': ['https://www.youtube.com/watch?v=ZXsQAXx_ao0'], //currently the shialebouf video
+  'IdkYouTellMe': ['https://csx.codesmith.io/home'], //currently the CSX homepage
+  'annoyed'['https://www.youtube.com/watch?v=dQw4w9WgXcQ','https://thatsthefinger.com/'] //rickroll video and a middle finger gif
 }
 
 // get a feeling
 //   in random function we get a random number between 1 and Ten
 // this corresponds to array we want based on
 // Math.random() * lengthoffeelingarrs
+
+
+
+const sad = url => {
+  //do something sad then go to a link in sad array
+}
+
+const happy = url => {
+  //do something happy then go to a link in happy array
+}
+const motivated = url => {
+  //do something motivating then go to a link in motivated array
+}
+const IdkYouTellMe = url => {
+  alert('what am I a google chrome extension...')
+}
+const annoyed = url => {
+  let count = 0; 
+  while(count <15){
+    alert('ARE YOU ANNOYED YET!!!!!')
+  }
+}
+
+
 
 
 
@@ -48,10 +78,7 @@ website of puppy gifs
 
 like a want to go to a concert[enter: your favorite band ==> redirects to youtube / theband ]
 
-motivated {
-  shia lebouf video - just do it  
-  https://www.youtube.com/watch?v=ZXsQAXx_ao0
-}
+
 
 sad[
   // aspca commerical
@@ -67,5 +94,5 @@ https://www.forbes.com/real-time-billionaires/#2995e963d788
 ]
 
 annoyed[
-  rick roll video on youtube
+  'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
 ]
